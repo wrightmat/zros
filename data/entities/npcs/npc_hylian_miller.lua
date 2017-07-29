71e08506d56aed5484c9f3d73a83c97d7dc6e1a1
Matt <wright.matt.a@gmail.com>
2017-07-29 07:42:33 -0500

    Initial commits

---
+++ data/entities/npcs/npc_hylian_miller.lua
@@ -0,0 +1,46 @@
+local entity = ...
+local game = entity:get_game()
+sol.main.load_file("entities/generic_npc")(entity)
+
+local dialogs = { "greetings.generic", "all_characters.aerla", }
+game:set_value(entity:get_name() .. "_dialogs", tableToString(dialogs))
+game:set_value(entity:get_name() .. "_memory", 12) -- Number of dialogs this NPC can learn
+
+-- NPC Description...
+
+function entity:on_created()
+  entity:on_generic_created()
+  -- Creation event(s)
+  -- For example, setting the starting position/routine of the NPC when the
+  -- map loads (if it varies do to different routines) would go here.
+  random_walk()
+  if hour == 0 then
+
+  elseif hour == 1 then
+
+  elseif hour == 2 then
+
+  elseif hour == 3 then
+
+  elseif hour == 4 then
+
+  elseif hour == 5 then
+
+  elseif hour == 6 then
+
+  elseif hour == 7 then
+
+  elseif hour == 8 then
+
+  elseif hour == 9 then
+
+  elseif hour == 10 then
+
+  elseif hour == 11 then
+
+  end
+end
+
+function entity:on_interaction()
+  entity:on_generic_interaction()
+end
---
+++ data/entities/npcs/npc_hylian_virgator.lua
@@ -0,0 +1,93 @@
+local entity = ...
+local game = entity:get_game()
+local hour = game:get_value("hour_of_day")
+local last_hour
+sol.main.load_file("entities/generic_npc")(entity)
+
+local dialogs = { "greetings.generic", "all_characters.jhoro_ethern", }
+game:set_value(entity:get_name() .. "_dialogs", tableToString(dialogs))
+game:set_value(entity:get_name() .. "_memory", 12) -- Number of dialogs this NPC can learn
+
+-- Hylian Virgator/Landowner (name?)
+-- Walks out of his house in the morning, then through town to
+-- each of his farms throughout the day. Returns home in the evening.
+
+function entity:determine_routine()
+  if hour == 7 then
+    follow_path(entity, "wp_npc_virgator_1", function()
+      entity:get_movement():stop()
+      entity:get_sprite():set_animation("stopped")
+      entity:get_sprite():set_direction(2) -- Face to the left.
+    end)
+  elseif hour == 8 then
+    follow_path(entity, "wp_npc_virgator_2/8")
+  elseif hour == 9 then
+    follow_path(entity, "wp_npc_virgator_3", function()
+      entity:get_movement():stop()
+      entity:get_sprite():set_animation("stopped")
+      entity:get_sprite():set_direction(2) -- Face to the left.
+    end)
+  elseif hour == 10 then
+    follow_path(entity, "wp_npc_virgator_4")
+  elseif hour == 11 then
+    follow_path(entity, "wp_npc_virgator_5", function()
+      entity:get_movement():stop()
+      entity:get_sprite():set_animation("stopped")
+      entity:get_sprite():set_direction(2) -- Face to the left.
+    end)
+  elseif hour == 12 then
+    follow_path(entity, "wp_npc_virgator_6")
+  elseif hour == 13 then
+    follow_path(entity, "from_shop")
+  elseif hour == 14 then
+    follow_path(entity, "wp_npc_virgator_7")
+  elseif hour == 15 then
+    follow_path(entity, "wp_npc_virgator_2/8")
+  elseif hour == 16 then
+    follow_path(entity, "from_virgator", function() entity:remove() end)
+  end
+end
+
+function entity:on_created()
+  entity:on_generic_created()
+
+  if hour <= 6 then
+    entity:remove() -- NPC is inside his house.
+  elseif hour == 7 then
+    entity:set_position(game:get_map():get_entity("from_virgator"):get_position())
+  elseif hour == 8 then
+    entity:set_position(game:get_map():get_entity("wp_npc_virgator_1"):get_position())
+  elseif hour == 9 then
+    entity:set_position(game:get_map():get_entity("wp_npc_virgator_2/8"):get_position())
+  elseif hour == 10 then
+    entity:set_position(game:get_map():get_entity("wp_npc_virgator_3"):get_position())
+  elseif hour == 11 then
+    entity:set_position(game:get_map():get_entity("wp_npc_virgator_4"):get_position())
+  elseif hour == 12 then
+    entity:set_position(game:get_map():get_entity("wp_npc_virgator_5"):get_position())
+  elseif hour == 13 then
+    entity:set_position(game:get_map():get_entity("wp_npc_virgator_6"):get_position())
+  elseif hour == 14 then
+    entity:set_position(game:get_map():get_entity("from_shop"):get_position())
+  elseif hour == 15 then
+    entity:set_position(game:get_map():get_entity("wp_npc_virgator_7"):get_position())
+  elseif hour == 16 then
+    entity:set_position(game:get_map():get_entity("wp_npc_virgator_2/8"):get_position())
+  elseif hour >= 17 then
+    entity:remove() -- NPC is inside his house.
+  end
+
+  self:determine_routine()
+  sol.timer.start(self, 5000, function()
+    hour = game:get_value("hour_of_day")
+    if hour ~= last_hour then
+      self:determine_routine()
+    end
+    last_hour = hour
+    return true
+  end)
+end
+
+function entity:on_interaction()
+  entity:on_generic_interaction()
+end
