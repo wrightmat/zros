26c9822efbf0535036adf6a0e1dd95817a862dcc
Matt <wright.matt.a@gmail.com>
2017-12-28 23:56:42 -0600

    Collectible food

---
+++ data/items/food_meat_2.lua
@@ -0,0 +1,5 @@
+local item = ...
+local behavior = require("items/generic/collectible_food")
+
+local properties = {}
+behavior:create(item, properties)
---
+++ data/items/food_mushroom_1.lua
@@ -0,0 +1,8 @@
+local item = ...
+local behavior = require("items/generic/collectible_food")
+
+-- Mushrooms
+-- Variants: Zapshroom, Chillshroom, Sunshroom, Hearty Truffle
+
+local properties = {}
+behavior:create(item, properties)
