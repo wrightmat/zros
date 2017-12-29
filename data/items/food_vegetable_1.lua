c0f2d2ef243f4c8ad28f3de4b603b28102fee59a
Matt <wright.matt.a@gmail.com>
2017-12-28 23:57:08 -0600

    Collectible food

---
+++ data/items/food_vegetable_1.lua
@@ -0,0 +1,8 @@
+local item = ...
+local behavior = require("items/generic/collectible_food")
+
+-- Greens (Plants)
+-- Variants: Hylian Wheat, Electric Saffina, Cool Saffina, Warm Saffina
+
+local properties = {}
+behavior:create(item, properties)
