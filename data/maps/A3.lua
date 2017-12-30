a0e7470629f00ac3ee01333a272caac979b925d5
Matt <wright.matt.a@gmail.com>
2017-12-30 09:21:35 -0600

    Maps

---
+++ data/maps/A3.lua
@@ -0,0 +1,12 @@
+local map = ...
+local game = map:get_game()
+
+-- Event called at initialization time, as soon as this map is loaded.
+function map:on_started()
+  -- Desert is cool at night and warm during the day, sand kicks up during the day too.
+  if game:get_time_of_day() == "night" then
+    self.properties = { "sand", 0.7, "cool", "mayfly", nil }
+  else
+    self.properties = { "sandstorm", 0.9, "warm", "mayfly", nil }
+  end
+end
