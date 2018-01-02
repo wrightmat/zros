efb920fcb5ef8368aacb648148268d6f9d439ba0
Matt <wright.matt.a@gmail.com>
2018-01-01 21:47:46 -0600

    Maps

--- data/maps/C3.lua
+++ data/maps/C3.lua
@@ -3,5 +3,5 @@ local game = map:get_game()
 
 -- Event called at initialization time, as soon as this map is loaded.
 function map:on_started()
-  self.properties = { nil, 1, "temperate", "butterfly", "mushrooms" }
+  self.properties = { "", 0, "temperate", "butterfly", "mushrooms" }
 end
